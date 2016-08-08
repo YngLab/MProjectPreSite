11da16a7f84bb287d783a43438fcf7a1c90dcdce
sKawashima <bysk.333@gmail.com>
2016-08-09 08:47:07 +0900

    文面調整

--- php/mail.php
+++ php/mail.php
@@ -1,27 +1,55 @@
 <?php
 session_start();
 
-$add_header = "From:bysk.333@gmail.com\n";//送信者の情報(メールヘッダー)
-$add_header .= "Reply-to: bysk.333@gmail.com\n";//送信者の情報(メールヘッダー)
+$add_header = "From: info@hakodate-miraiproject.jp\n";//送信者の情報(メールヘッダー)
+$add_header .= "Reply-to: info@hakodate-miraiproject.jp\n";//送信者の情報(メールヘッダー)
 $add_header .= "X-Mailer: PHP/". phpversion();
-$opt = '-f'.'bysk.333@gmail.com'; //-fすると迷惑メールになりにくいとか、そんなことだったと思う。送信エラーの時にエラーメールを返す先
+
+$return =<<<HTML
+お問い合わせありがとうございます。
+
+お名前：
+{$_SESSION['name']}
+
+E_mail：
+{$_SESSION['mail']}
+
+お問い合わせ内容：
+{$_SESSION['message']}
+
+内容確認後、担当者より折り返しご連絡をさせていただきます。
+
+-----
+はこだてみらいプロジェクト（はこだてみらい館・はこだてキッズプラザ）
+
+〒040-0063函館市若松町20番1号 キラリス函館
+TEL : 0138-23-1131(NAアーバンデベロップメント)
+E-Mail : info@hakodate-miraiproject.jp
+HTML;
 
 $message =<<<HTML
-お問い合わせ内容の確認です。
-お名前
+お問い合わせ内容は以下の通りです。
+
+お名前：
 {$_SESSION['name']}
-E_mail
+
+E_mail：
 {$_SESSION['mail']}
-お問い合わせ内容
+
+お問い合わせ内容：
 {$_SESSION['message']}
-内容確認後、担当者より折り返しご連絡をさせていただきます。
+
+内容確認後、折り返し連絡をお願いします。
 HTML;
 
 mb_language("ja");
-if(mb_send_mail($_SESSION['mail'],"【お問い合わせ】確認メール",$message,$add_header)&&mb_send_mail('bys.k333@gmail.com',"問い合わせがありました",$message,$add_header)){
+mb_internal_encoding("UTF-8");
+
+if(mb_send_mail($_SESSION['mail'],"【お問い合わせ】確認メール",$return,$add_header)&&mb_send_mail('info@hakodate-miraiproject.jp',"問い合わせがありました",$message,$add_header)){
 
 $contents = <<<END
-5秒後に <a href="../">元のページ</a> に移動します。<br />
+送信が完了しました。<br>
+5秒後に <a href="../">元のページ</a> に移動します。<br>
 <script type="text/javascript">
 <!--
 setTimeout(function(){
