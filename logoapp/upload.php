98e5679de9ac5a3309bf86fe071906634aa48824
sKawashima <bysk.333@gmail.com>
2016-09-03 01:24:36 +0900

    ファイル名を変数に定義

--- logoapp/upload.php
+++ logoapp/upload.php
@@ -19,12 +19,14 @@ if($file_get_contents = file_get_contents($_POST["data"])){
 $contents_split = explode(',', $data);
 $encoded = $contents_split[count($contents_split)-1];
 $decoded = "";
+$fileName= "test";
+
 for ($i=0; $i < ceil(strlen($encoded)/256); $i++) {
-  $decoded = $decoded . base64_decode(substr($encoded,$i*256,256)); 
+  $decoded = $decoded . base64_decode(substr($encoded,$i*256,256));
 }
 
 //echo $decoded;
 
-$fp = fopen('upload/imagembaixada.gif', 'w');
+$fp = fopen("upload/".$fileName.".gif", 'w');
 fwrite($fp, $decoded);
 fclose($fp);
